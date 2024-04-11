393dfb8046ee5ab53384280a91cdb19e182b5dd2
Suhyun Kim <suhyunroom@naver.com>
2024-04-11 19:20:13 +0900

    first commit

---
+++ src/main/java/owner/ownerfrontend/OwnerFrontendApplication.java
@@ -0,0 +1,13 @@
+package owner.ownerfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/owner/ownerfrontend/OwnerFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package owner.ownerfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
